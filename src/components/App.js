8c53732beca55a97b269140a472fa3df79b700c5
flakster <carlos.santamaria@gmail.com>
2021-02-09 10:56:31 -0500

    Implement App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+function App(){
+  render(){
+    return(
+      <React.Fragment>
+        <Display />
+        <ButtonPannel />
+      </React.Fragment>
+    )
+  }
+}
