4497146938f4aaa382c69cc7809ecdca65bd0c4d
flakster <carlos.santamaria@gmail.com>
2021-02-16 10:57:44 -0500

    Wrap Display and ButtonPanel components into a fragment

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 const App = () => (
-  <div className="App">
+  <>
     <Display />
     <ButtonPanel />
-  </div>
+  </>
 );
 
 export default App;
