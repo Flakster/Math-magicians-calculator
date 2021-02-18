988f3b58797161f80a64731437ef1ee82a7a6150
flakster <carlos.santamaria@gmail.com>
2021-02-18 12:00:06 -0500

    Import operate module from the calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,9 +1,20 @@
+import operate from './operate';
+
 const calculate = (calculator, buttonName) => {
   const { total, next, operation } = calculator;
+  const operators = ['+', '-', 'x', '÷', '%'];
   if (buttonName === '+/-') {
     return {
       total: (total * -1),
       next: (next * -1),
+      operation,
+    };
+  }
+
+  if (operators.includes(buttonName)) {
+    return {
+      total: operate(total, next, buttonName),
+      next,
       operation,
     };
   }
